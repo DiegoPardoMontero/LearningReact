125ee7a345b2b87fd5adfb07489c8fba393890af
Diego Pardo Montero <diegopardomontero@gmail.com>
2024-06-08 21:26:59 -0500

    Developed example of JSX

---
+++ ReactDocs/JSX-example.js
@@ -0,0 +1,10 @@
+//Really simple component
+//However, it shows you can use JSX for creating reusable components
+function AboutPage() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>Hello there.<br />How do you do?</p>
+      </>
+    );
+  }
