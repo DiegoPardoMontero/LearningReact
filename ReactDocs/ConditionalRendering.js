764459893aa076917299bd946bb2d6722377d6cb
Diego Pardo Montero <diegopardomontero@gmail.com>
2024-06-08 21:32:41 -0500

    Add Conditional Rendering example

---
+++ ReactDocs/ConditionalRendering.js
@@ -0,0 +1,13 @@
+//Returns different JSX tag depending on a condition
+//that may have gotten from some data 
+let content;
+if (isLoggedIn) {
+  content = <AdminPanel />;
+} else {
+  content = <LoginForm />;
+}
+return (
+  <div>
+    {content}
+  </div>
+);
