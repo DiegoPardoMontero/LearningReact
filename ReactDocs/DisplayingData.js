9a239d533ef8e56bbce2b1312732e961adb340ee
Diego Pardo Montero <diegopardomontero@gmail.com>
2024-06-08 21:28:45 -0500

    Add a component with data from JS

---
+++ ReactDocs/DisplayingData.js
@@ -0,0 +1,25 @@
+//You can use objects and variables to get data into the page
+//through JSX. Even atributes can be filled with this data
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
+  export default function Profile() {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
+  
